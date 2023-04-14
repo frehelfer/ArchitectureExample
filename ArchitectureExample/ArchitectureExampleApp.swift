56e195931bb8a9b10f2db27592559a1a747afdd1
Frédéric Helfer <fre.helfer@gmail.com>
2023-04-14 09:09:15 -0300

    Initial Commit

---
+++ ArchitectureExample/ArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureExampleApp.swift
+//  ArchitectureExample
+//
+//  Created by Frédéric Helfer on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
